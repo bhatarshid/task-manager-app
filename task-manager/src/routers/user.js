99bed5e33d92adbac11a7a0c15575e0962b1cb56
Arshid <bhatarshid1000@gmail.com>
2023-03-14 16:54:16 +0530

    hide token and password

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -28,7 +28,8 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
+        const token = await user.generateAuthToken() 
+
         res.send({ user, token })
     }
     catch (e) {
