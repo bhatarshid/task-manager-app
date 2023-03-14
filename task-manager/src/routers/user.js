744db2439be7facd963b029c1f5bb73ed2354f88
Arshid <bhatarshid1000@gmail.com>
2023-03-14 17:28:08 +0530

    update and delete user profile

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -72,34 +72,34 @@ router.get('/users/me', auth, async (req, res) => {
 })
 
 //get user by id
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
+// router.get('/users/:id', async (req, res) => {
+//     const _id = req.params.id
 
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    }
-    catch {
-        res.status(500).send()
-    }
+//     try {
+//         const user = await User.findById(_id)
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+//         res.send(user)
+//     }
+//     catch {
+//         res.status(500).send()
+//     }
 
     
-    // User.findById(_id).then((user) => {
-    //     if(!user){
-    //         return res.status(404).send()
-    //     }
+//     // User.findById(_id).then((user) => {
+//     //     if(!user){
+//     //         return res.status(404).send()
+//     //     }
         
-    //     res.send(user)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
+//     //     res.send(user)
+//     // }).catch((e) => {
+//     //     res.status(500).send()
+//     // })
+// })
 
 //update user
-router.patch('/users/:id', async (req,res) => {
+router.patch('/users/me', auth, async (req,res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -109,34 +109,22 @@ router.patch('/users/:id', async (req,res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id)
-        //iterate the list updates to check which properties are updates and apply them
-        updates.forEach( (update) => user[update] = req.body[update])
-        await user.save()
-
-        // below line of code can be used if we donot use bcrypt for passwords
-        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
         
-        if(!user){
-            return res.send(404).send()
-        }
+        //iterate the list updates to check which properties are updates and apply them
+        updates.forEach( (update) => req.user[update] = req.body[update])
+        await req.user.save()
 
-        res.send(user)
+        res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
 //delete user
-router.delete('/users/:id', async (req,res) => {
+router.delete('/users/me', auth, async (req,res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        
-        if(!user){
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        await User.deleteOne(req.user)
+        res.send(req.user)
     } catch (e) {
         res.status(500).send(e)
     }
