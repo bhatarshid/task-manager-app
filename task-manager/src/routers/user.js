a43b472986b0788a76ce0f942764e7598619cc70
Arshid <bhatarshid1000@gmail.com>
2023-03-15 12:43:09 +0530

    upload file

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router
@@ -128,6 +129,16 @@ router.delete('/users/me', auth, async (req,res) => {
     } catch (e) {
         res.status(500).send(e)
     }
+})
+
+//upload profile photo
+
+const upload = multer({
+    dest: 'avatars'
+})
+
+router.post('/users/me/avatar', upload.single('avatar') ,(req, res) => {
+    res.send()
 })
 
 module.exports = router
