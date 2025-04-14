f805a1487969921c1f7d8dd2e4360ef3c8dbda56
arshid.bhat <bhatarshid1000@gmail.com>
2025-04-14 12:17:58 +0530

    navbar fix

--- task-manager-client/src/components/navbar.jsx
+++ task-manager-client/src/components/navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex flex-row justify-between border-b-1 rounded-b-lg px-5 py-3 fixed top-0 left-0 right-0 shadow-md bg-black'>
-      <h1 className='text-2xl font-bold text-center'>Task Manager</h1>
-      <div>
+    <div className='flex flex-row justify-between border-b-1 rounded-b-lg px-5 py-1 sm:py-3 fixed top-0 left-0 right-0 shadow-md bg-black'>
+      <h1 className='text-sm sm:text-2xl font-semibold sm:font-bold text-center'>Task Manager</h1>
+      <div className='flex space-x-4'>
         <button>
           <a href="/login" className='text-white font-medium py-2 px-4 rounded'>Login</a>
         </button>
-        <button>
+        <button className='hidden sm:block'>
           <a href="/signup" className='text-black font-medium py-2 px-4 bg-white rounded'>
             Sign Up
           </a>
