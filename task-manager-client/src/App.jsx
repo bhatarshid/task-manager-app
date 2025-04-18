7f9a22e648338aa02b62d6b672bae626c7226c26
arshid.bhat <arshid.bhat@hexaviewtech.com>
2025-04-18 11:53:06 +0530

    prod issue

--- task-manager-client/src/App.jsx
+++ task-manager-client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
-import Signup from "./pages/signup"
+import Signup from "./pages/Signup"
 import Dashboard from "./pages/Dashboard"
 import Profile from "./pages/Profile"
 import NotFound from "./pages/NotFound"
